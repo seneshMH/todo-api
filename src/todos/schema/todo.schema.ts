6a605766d916839ecbf9ccec39694d8ae16b89e1
senesh <senesh1senesh@gmail.com>
2024-03-25 02:18:49 +0530

    create todo schema

---
+++ src/todos/schema/todo.schema.ts
@@ -0,0 +1,15 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type TodoDocument = HydratedDocument<Todo>;
+
+@Schema({ timestamps: true })
+export class Todo {
+    @Prop()
+    title: string;
+
+    @Prop()
+    body: string;
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo);
