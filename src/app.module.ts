73ddaa2d826e25046092b99b3973c12037377ab7
senesh <senesh1senesh@gmail.com>
2024-03-25 02:15:45 +0530

    setup mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosModule } from './todos/todos.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    //ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://0.0.0.0:27017/todo'),
+    TodosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
