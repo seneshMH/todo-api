1970c81add1faee1e12369ce1ffd08f4b7e40c4c
senesh <senesh1senesh@gmail.com>
2024-03-25 02:30:28 +0530

    fix MongooseModule imports

--- src/todos/todos.module.ts
+++ src/todos/todos.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { TodosController } from './todos.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Todo, TodoSchema } from './schema/todo.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }])],
   controllers: [TodosController],
   providers: [TodosService],
 })
-export class TodosModule {}
+export class TodosModule { }
